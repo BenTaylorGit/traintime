5bf55d60ea4ac28ba18ee6b349fc370bed31c25b
Ben Taylor <bentaylor@knights.ucf.edu>
2020-02-29 10:45:08 -0500

    Added hookup to firebase database, added functionality, to add info to table, next will add the time math and moment() info

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,7 @@ var config = {
   };
   
   firebase.initializeApp(config);
-  
+  var trainInfo = firebase.database();
 
   $("#add-btn").on("click", function(event) {
 
@@ -23,7 +23,7 @@ var config = {
     var destination = $("#user-destination-name")
       .val()
       .trim();
-    var firstTrain = $("#user-train-time")
+    var trainTime = $("#user-train-time")
       .val()
       .trim();
     var frequency = $("#user-frequency-time")
@@ -31,7 +31,34 @@ var config = {
       .trim();
  console.log(trainName);
   console.log(destination);
-  console.log(firstTrain);
+  console.log(trainTime);
   console.log(frequency);
+
+  var newTrainObject = {
+    name: trainName,
+    destination: destination,
+    trainTime: trainTime,
+    frequency: frequency
+  };
+
+  trainInfo.ref().push(newTrainObject);
   
   });
+
+  trainInfo.ref().on("child_added", function(childSnapshot, prevChildKey) {
+    console.log(childSnapshot.val());
+  
+
+    var newTrainName = childSnapshot.val().name;
+    var newTrainDestination = childSnapshot.val().destination;
+    var newTrainFrequency = childSnapshot.val().frequency;
+   
+
+    $("#train-sched > tbody").append(
+        $("<tr>").append(
+          $("<td>").text(newTrainName),
+          $("<td>").text(newTrainDestination),
+          $("<td>").text(newTrainFrequency),
+        )
+      );
+    });
