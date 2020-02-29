918ce9338bebffc48e54798cde30f1f87890f158
Ben Taylor <bentaylor@knights.ucf.edu>
2020-02-29 10:06:07 -0500

    Added firebase config to js file

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+
+var config = {
+   
+        apiKey: "AIzaSyAofkZwpLbzKA_YCEy0K3KBO3owFuhXCLs",
+        authDomain: "traintime-31e75.firebaseapp.com",
+        databaseURL: "https://traintime-31e75.firebaseio.com",
+        projectId: "traintime-31e75",
+        storageBucket: "traintime-31e75.appspot.com",
+        messagingSenderId: "251177396006",
+        appId: "1:251177396006:web:0ca39caec7e2a13a3f83e7"
+     
+  };
+  
+  firebase.initializeApp(config);
+  
+  
